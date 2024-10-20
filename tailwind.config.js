55f45d6131e180b92edaf18ffe8bdf2e0a8aae11
bouhraoua <akram.bouhraoua@etu.univ-amu.fr>
2024-10-20 14:31:38 +0200

    Added landing page structure with Tailwind Starter Kit integration
    - Implemented basic HTML structure for the landing page using Tailwind CSS.
    - Integrated Creative Tim's Tailwind Starter Kit for responsive design elements.
    - Added navigation bar with links to "Docs" and social media buttons (Facebook, Twitter).
    - Created main content sections for services including "Graphic Design", "Artistic Illustrations", and "Web Design".
    - Included "About Us" section with details about the team and services offered.
    - Incorporated SVG images for stylistic separation between sections.
    - Linked external stylesheets, font libraries, and icon packs (Font Awesome).

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["Landing.html"],
+  theme: {
+    extend:{
+      fontFamily: {
+        holimount: ['Holimount'],
+      },
+    },
+  },
+  plugins: [],
+}
+
